f20fee919551d5bf5156d9bd950f4c68eafb765a
Corval-LC <alejandromcorva@gmail.com>
2026-05-10 10:30:06 -0300

    feat(ui): implementar vistas responsivas, formulario reactivo de asesor y nuevo layout de login

---
+++ Frontend/src/app/pages/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  templateUrl: './login.component.html',
+  host: {
+    'class': 'w-full flex flex-grow'
+  }
+})
+export class LoginComponent {
+}
